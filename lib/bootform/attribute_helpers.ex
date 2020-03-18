eb4f047aab9a4a06e0881ee30340863b41b33630
Nicolas Blanco <me@nicolasblan.co>
2020-03-18 22:31:10 +0200

    Fix when form name is nil

--- lib/bootform/attribute_helpers.ex
+++ lib/bootform/attribute_helpers.ex
@@ -2,7 +2,6 @@ defmodule Bootform.AttributeHelper do
 
    @spec id(Phoenix.HTML.Form.t, Atom.t) :: String.t
    def id(%Phoenix.HTML.Form{name: name}, field) do
-     name <> String.capitalize(Atom.to_string(field))
+     "#{name}#{String.capitalize(Atom.to_string(field))}"
    end
-
 end
